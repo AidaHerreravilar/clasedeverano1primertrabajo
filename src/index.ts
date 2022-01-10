495fb7f60e5bf27b3deb7513869b43a2c78cc187
AidaHerreravilar <92064121+AidaHerreravilar@users.noreply.github.com>
2022-01-10 20:13:09 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log ("Supermercado Aida");
+console.log ("Fideo Sopero Maiolo");
+console.log ("Fideos Soperos Matarazzo");
+console.log ("Galletitas Surtidas Diversion");
+console.log ("Azucar Domino");
+console.log ("Arroz Grano Largo Gallo");
+console.log("Arvejas Arcor");
+console.log ("Imprimir");
